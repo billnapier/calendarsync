ef36c23f76a80d439d275d6082ad851ad9337b15
Bill Napier <napier@pobox.com>
2025-12-13 23:58:53 -0800

    Add Pylint and GitHub Action workflow

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
-import pytest
 import sys
 import os
+import pytest
+# pylint: disable=wrong-import-position
+# pylint: disable=redefined-outer-name
 
 # Add the app directory to the path so we can import the app
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
